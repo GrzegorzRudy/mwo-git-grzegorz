f27ce6ad600c23f685a0c60c4ea78e8bf2d64f11
Mateusz Pietrzyk <mateusz.pietrzyk@gmail.com>
2024-01-20 11:38:32 +0100

    New Commit Rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,5 +3,10 @@ public class SomeProgram {
     System.out.println("We will be Git experts, soon.");
       System.out.println("We are learning to use Git."); 
     }
+
+public void manageTheRockets() {
+    // TODO implement
+}
+
 }
 
